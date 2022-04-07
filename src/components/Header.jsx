47a7b285e08d35454df5f155aa304f79add822a7
Jasimcuh <jasim.cuh@gmail.com>
2022-04-07 13:13:39 +0530

    feedback UI First Commint without db.json

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types'
+
+function Header({text,bgColor,textColor}) {
+    const headerStyles = {
+        backgroundColor: bgColor,
+        color: textColor
+    }
+  return (
+    <header style={headerStyles}>
+        <div className="container">
+            <h2>{text}</h2>
+        </div>
+    </header>
+  )
+}
+
+// Default Props
+Header.defaultProps = {
+    text: 'Feedback UI',
+    bgColor: 'rgba(0,0,0,0.4)',
+    textColor: '#fff6a95'
+}
+
+//Props Type Check
+Header.propTypes = {
+    text: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string
+}
+
+export default Header
